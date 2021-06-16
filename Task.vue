6c3b68728c93c85a7bf6d9a0133996ea00ba892b
500apps-jagadeeshkuna <85148614+jagadeeshkuna@users.noreply.github.com>
2021-06-16 19:08:07 +0530

    Add files via upload

---
+++ Task.vue
@@ -0,0 +1,17 @@
+<template>
+<div class="task">
+
+</div>
+</template>
+<script>
+
+export default {
+    name: 'Task',
+    props: {
+        tasks: Array,
+    },
+}
+</script>
+<style>
+
+</style>
---
+++ Tasks.vue
@@ -0,0 +1,40 @@
+<template>
+<div>
+ <div  :key="task.id" v-for="task in tasks">
+         <Task :task="task"/>
+         <h3>{{task.text}}</h3>
+<p>{{task.day}}</p>
+</div>
+</div>
+</template>
+
+<script>
+import Task from './Task'
+export default{
+    name: 'Tasks',
+    props: {
+        tasks: Array,
+        },
+        components: {
+            Task,
+        }
+
+}
+</script>
+
+<style>
+.task {
+    background: #f4f4f4;
+    margin: 5px;
+    padding: 10px 20px;
+    cursor: pointer;
+}
+.task.remainder {
+    border-left: 5px solid black;
+}
+.task h3 {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+</style>
